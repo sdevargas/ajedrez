2bb270132fb971275892d8e1a03727bbc6133679
sdevargas <s.devargas@alumnos.upm.es>
2024-04-21 17:07:27 +0200

    Matriz de punteros a Pieza
    #Se crea y configura una matriz que contendrá las piezas del tablero
    #Se asigna la posición inicial de las piezas y en el resto nullptr
    #Se dibujan las piezas (como esferas) en su posición

--- src/Tablero.cpp
+++ src/Tablero.cpp
@@ -1,8 +1,51 @@
 #include "Tablero.h"
 #include <freeglut.h>
 
+Tablero::Tablero(){
+	//Hacemos nullptr todas las casillas para que no haya basura
+	for (int columna = 0; columna < limite_columnas; columna++) {
+		for (int fila = 0; fila < limite_filas; fila++) {
+			PosicionPiezas[columna][fila] = nullptr;
+		}
+	}
+	//Inicializamos en la matriz las posiciones de partida {PosicionPieza[i][j]= new "constructor del tipo de pieza" }
+	/*Se hace de manera denámica porque no siempre tendrás las mismas piezas*/
+	//PEONES
+	for (int columna = 2; columna < 9; columna++) {
+		PosicionPiezas[columna][2] = new Peon(Pieza::BLANCO);
+	}
+	for (int columna = 2; columna < 9; columna++) {
+		PosicionPiezas[columna][7] = new Peon(Pieza::NEGRO);
+	}
+	//TORRES
+	PosicionPiezas[3][1] = new Torre(Pieza::BLANCO);
+	PosicionPiezas[7][1] = new Torre(Pieza::BLANCO);
+	PosicionPiezas[3][8] = new Torre(Pieza::NEGRO);
+	PosicionPiezas[7][8] = new Torre(Pieza::NEGRO);
+	//CABALLOS
+	PosicionPiezas[4][1] = new Caballo(Pieza::BLANCO);
+	PosicionPiezas[6][1] = new Caballo(Pieza::BLANCO);
+	PosicionPiezas[4][8] = new Caballo(Pieza::NEGRO);
+	PosicionPiezas[6][8] = new Caballo(Pieza::NEGRO);
+	//ALFILES
+	PosicionPiezas[5][1] = new Alfil(Pieza::BLANCO);
+	PosicionPiezas[5][0] = new Alfil(Pieza::BLANCO);
+	PosicionPiezas[5][8] = new Alfil(Pieza::NEGRO);
+	PosicionPiezas[5][9] = new Alfil(Pieza::NEGRO);
+	//REYES
+	PosicionPiezas[4][0] = new Rey(Pieza::BLANCO);
+	PosicionPiezas[4][9] = new Rey(Pieza::NEGRO);
+	//REINAS
+	PosicionPiezas[6][0] = new Reina(Pieza::BLANCO);
+	PosicionPiezas[6][9] = new Reina(Pieza::NEGRO);
+
+
+
+	this->turno = BLANCAS;
+}
+
 void Tablero::Dibuja(){
-;
+	//DIBUJO DE LAS CASILLAS
 	for (int columna = 0; columna < limite_columnas; columna++) {
 		for (int fila = 0; fila < limite_filas; fila++) {
 			if (fila + columna > 15 || fila + columna < 4 ||	
@@ -22,6 +65,17 @@ void Tablero::Dibuja(){
 				glEnd();
 				glEnable(GL_LIGHTING);
 			
+			}
+		}
+	}
+
+	//DIBUJO DE LAS PIEZAS
+	for (int columna = 0; columna < limite_columnas; columna++) {
+		for (int fila = 0; fila < limite_filas; fila++) {
+			if (PosicionPiezas[columna][fila] != nullptr) {
+				glTranslatef(columna +0.5, fila+0.5, 0); //Desplazas el eje de cordenadas
+				PosicionPiezas[columna][fila]->Dibuja(); //Llamada al método Dibuja de la pieza que corresponde
+				glTranslatef(-columna - 0.5, -fila - 0.5, 0);
 			}
 		}
 	}
--- src/Tablero.h
+++ src/Tablero.h
@@ -1,8 +1,29 @@
 #pragma once
+#include "Pieza.h"
+#include"Vector2D.h"
+#include "Peon.h"
+#include "Torre.h"
+#include "Caballo.h"
+#include "Rey.h"
+#include "Reina.h"
+#include"Alfil.h"
+
 #define limite_filas 10
 #define limite_columnas 11
 
 class Tablero {
 public:
+	typedef enum { BLANCAS = 0, NEGRAS } Turno;
+
+private:
+	Turno turno;
+
+public:
+	Pieza* PosicionPiezas[limite_columnas][limite_filas]; //Se crea una matriz para albergar la posición de las piezas en el tablero
+/*Se necesita una matriz de piezas para registrar dónde se encuentran las piezas en el tablero
+Con esa matriz verificas que puedes acceder a la casilla de destino */
+
+	Tablero();
 	void Dibuja();
+
 };
